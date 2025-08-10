d328b415906cf6906370fdfbcef3a8fa39dab7f0
Daniel G Szabo <d.g.szabo-2017@hull.ac.uk>
2025-08-10 14:47:36 +0100

    feat: add role column to users table with default value 'job_seeker'

---
+++ database/migrations/2025_08_10_133858_add_role_column_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Add role column to users table
+            $table->string('role')->default('job_seeker');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Remove role column from users table
+            $table->dropColumn('role');
+        });
+    }
+};
